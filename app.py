b63bd5a8b6ffd8d6aa2475a146435e8f7f9df84e
rodri <rodrigo.oliveiracabral@hotmail.com>
2024-11-17 14:29:33 +0000

    Add endpoints for retrieving deputy expenditures by UF and name

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from src.API.Political.deputy import Deputy
+from src.API.Expenditures.expenditures import Expenditures
 import requests as req
+import json
 
 app = FastAPI()
 
@@ -27,3 +29,18 @@ def get_deputy_by_name(name: str):
 def get_deputy_expenditure():
     responses = get_all_deputy()
     return responses
+
+@app.get("/expenditures/UF={UF}")
+def get_deputy_expenditure_by_UF(UF: str):
+    responses = get_deputy_by_UF(UF)
+    return responses
+
+@app.get("/expenditures/name={name}")
+def get_deputy_expenditure_by_name(name: str):
+    responses = get_deputy_by_name(name)
+    deputy = Deputy()
+    ids = deputy.get_id_deputy(responses)
+    expenditures = Expenditures(ids[0]["id"])
+    response_expenditures = expenditures.get_deputy_expenditure()
+
+    return response_expenditures
